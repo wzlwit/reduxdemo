3380bdb21cc1d4baeb32853121034cc4705a01e5
Zhaolong Wang <zl.wangadfa@hotmail.com>
2019-02-23 21:28:10 -0500

    Updates

---
+++ precache-manifest.f6db4afde24dc7ee7415f83c0af63674.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "191423d2c0061dff60ea",
+    "url": "/reduxdemo/static/js/runtime~main.191423d2.js"
+  },
+  {
+    "revision": "0b354eb92b2c743c2ba8",
+    "url": "/reduxdemo/static/js/main.0b354eb9.chunk.js"
+  },
+  {
+    "revision": "73ea40deaf5347faadf3",
+    "url": "/reduxdemo/static/js/2.73ea40de.chunk.js"
+  },
+  {
+    "revision": "0b354eb92b2c743c2ba8",
+    "url": "/reduxdemo/static/css/main.c7a09ab7.chunk.css"
+  },
+  {
+    "revision": "2648f9a89aa00de7eab14c698c78a1d8",
+    "url": "/reduxdemo/index.html"
+  }
+];
